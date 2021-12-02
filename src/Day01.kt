873963b179150ca6762e2eccafdcade9b37b3f7f
mikayel <mikayel.aghababyan@gmail.com>
2021-12-02 14:32:49 -0500

    day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,28 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<Int>): Int {
+        return input.windowed(2).count { (a,b) -> a < b}
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<Int>): Int {
+        return input
+            .windowed(3)
+            .windowed(2)
+            .count { (a,b) -> a.sum() < b.sum()}
+    }
+
+    //Better solution:  A + B + C <=> B + C + D   is equivalent to comparing A <=> D
+    fun part2better(input: List<Int>): Int {
+        return input
+            .windowed(4)
+            .count { it[0] < it[3]}
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInputAsInts("Day01_test")
+    check(part1(testInput) == 7)
+    check(part2better(testInput) == 5)
 
-    val input = readInput("Day01")
+    val input = readInputAsInts("Day01")
     println(part1(input))
-    println(part2(input))
+    println(part2better(input))
 }
